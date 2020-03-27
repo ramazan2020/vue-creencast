13c99f0e7aae332aaa2647ac73e62af7ea93888a
ramazanpolat <ramazanpolat@DESKTOP-80P97TG>
2020-03-27 20:31:20 +0300

    .

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,18 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        test
-      </h1>
-      <h2 class="subtitle">
-        yeh
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class="title">Ramazan POLAT</h1>
+      <h2 class="subtitle">yeh</h2>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  components: {
-    Logo
-  }
-})
+  components: {}
+});
 </script>
 
 <style>
@@ -50,8 +26,8 @@ export default Vue.extend({
 }
 
 .title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
